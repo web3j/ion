531e22f325c128775161499a4bc895b9ca982ad7
ivaylo kirilov <iikirilov@gmail.com>
2019-01-18 19:08:34 +0000

    init

---
+++ src/main/java/org/web3j/sample/Application.java
@@ -0,0 +1,66 @@
+package org.web3j.sample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.sample.contracts.generated.Ion;
+import org.web3j.tx.gas.DefaultGasProvider;
+
+/**
+ * A simple web3j application that demonstrates a number of core features of web3j:
+ *
+ * <ol>
+ *     <li>Connecting to a node on the Ethereum network</li>
+ *     <li>Loading an Ethereum wallet file</li>
+ *     <li>Sending Ether from one address to another</li>
+ *     <li>Deploying the Ion smart contract to the network</li>
+ * </ol>
+ *
+ * <p>To run this demo, you will need to provide:
+ *
+ * <ol>
+ *     <li>Ethereum client (or node) endpoint. The simplest thing to do is
+ *     <a href="https://infura.io/register.html">request a free access token from Infura</a></li>
+ *     <li>A wallet file. This can be generated using the web3j
+ *     <a href="https://docs.web3j.io/command_line.html">command line tools</a></li>
+ *     <li>Some Ether. This can be requested from the
+ *     <a href="https://www.rinkeby.io/#faucet">Rinkeby Faucet</a></li>
+ * </ol>
+ *
+ * <p>For further background information, refer to the project README.
+ */
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) throws Exception {
+        new Application().run();
+    }
+
+    private void run() throws Exception {
+
+        // We start by creating a new web3j instance to connect to remote nodes on the network.
+        // Note: if using web3j Android, use Web3jFactory.build(...
+        Web3j web3j = Web3j.build(new HttpService(
+                "https://rinkeby.infura.io/<your token>"));  // FIXME: Enter your Infura token or node url here;
+        log.info("Connected to Ethereum client version: "
+                + web3j.web3ClientVersion().send().getWeb3ClientVersion());
+
+        // We then need to load our Ethereum wallet file
+        // FIXME: Generate a new wallet file using the web3j command line tools https://docs.web3j.io/command_line.html
+        Credentials credentials =
+                WalletUtils.loadCredentials(
+                        "<password>",
+                        "/path/to/<walletfile>");
+        log.info("Credentials loaded");
+
+        // Deploying Ion contract
+        Ion ion = Ion.deploy(
+                web3j, credentials,
+                new DefaultGasProvider(),
+                web3j.netVersion().send().getNetVersion().getBytes()).send();
+    }
+}
---
+++ src/test/java/org/web3j/sample/GreeterContractIT.java
@@ -0,0 +1,16 @@
+package org.web3j.sample;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Integration test to run our main application.
+ */
+@Ignore
+public class GreeterContractIT {
+
+    @Test
+    public void testGreeterContract() throws Exception {
+        Application.main(new String[]{ });
+    }
+}
